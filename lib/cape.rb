7f98a3c0276a283cb6324f5f7cd5007b48331f59
Nils Jonsson <nils@alumni.rice.edu>
2015-01-11 18:12:52 -0600

    Use Module#autoload to lazy-load nested source files

--- lib/cape.rb
+++ lib/cape.rb
@@ -1,9 +1,19 @@
-::Dir.glob(::File.expand_path('../cape/*.rb', __FILE__)) do |f|
-  require "cape/#{::File.basename f, '.rb'}"
-end
-
 # Contains the implementation of Cape.
 module Cape
+
+  autoload :Capistrano,                 'cape/capistrano'
+  autoload :CapistranoDeprecated,       'cape/capistrano_deprecated'
+  autoload :CoreExt,                    'cape/core_ext'
+  autoload :Deprecation,                'cape/deprecation'
+  autoload :DSL,                        'cape/dsl'
+  autoload :DSLDeprecated,              'cape/dsl_deprecated'
+  autoload :HashList,                   'cape/hash_list'
+  autoload :Rake,                       'cape/rake'
+  autoload :RecipeDefinition,           'cape/recipe_definition'
+  autoload :RecipeDefinitionDeprecated, 'cape/recipe_definition_deprecated'
+  autoload :Util,                       'cape/util'
+  autoload :VERSION,                    'cape/version'
+  autoload :XTerm,                      'cape/xterm'
 
   extend DSL
   extend DSLDeprecated
--- lib/cape/deprecation.rb
+++ lib/cape/deprecation.rb
@@ -1,12 +1,16 @@
-::Dir.glob(::File.expand_path('../deprecation/*.rb', __FILE__)) do |f|
-  require "cape/deprecation/#{::File.basename f, '.rb'}"
-end
-
 module Cape
 
   # Contains implementations of deprecation stream printers.
   #
   # @api private
-  module Deprecation; end
+  module Deprecation
+
+    autoload :Base, 'cape/deprecation/base'
+    autoload :CapistranoDeprecatedDefineRakeWrapper ,
+             'cape/deprecation/capistrano_deprecated_define_rake_wrapper'
+    autoload :DSLDeprecatedMirrorRakeTasks ,
+             'cape/deprecation/dsl_deprecated_mirror_rake_tasks'
+
+  end
 
 end
